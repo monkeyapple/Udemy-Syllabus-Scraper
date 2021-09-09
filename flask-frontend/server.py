6e7e814d6035770e3e7d7cbe619a953e7dd58d72
monkeyapple <monkeyappleh@gmail.com>
2021-09-09 17:35:54 +0800

    frontend layout

--- flask-frontend/server.py
+++ flask-frontend/server.py
@@ -1,12 +1,35 @@
-from flask import Flask,jsonify,request,redirect,url_for
-from flask.templating import render_template
+from flask import Flask,session,redirect,jsonify,request,redirect,url_for,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Length,Regexp
+
 app=Flask(__name__)
-@app.route('/')
+app.config['SECRET_KEY']='mysecretkey'
+
+###################################################
+##################### Form #######################
+###################################################
+class SearchForm(FlaskForm):
+    courseLink=StringField('Enter the course URL:',validators=[Regexp('https:\/\/www\.udemy\.com\/course\/[\d*\w*\-*]{1,256}\/',message="Please enter the correct Udemy course URL!")])
+    submit=SubmitField('Search')
+
+###################################################
+###################### View #######################
+###################################################
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    pass
+    form=SearchForm()
+    if form.validate_on_submit():
+        session['courseLink']=form.courseLink.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,urlQuery=session.get('courseLink',None))
 
 def submit():
     pass
 
 def scrape():
-    p
+    pass
+
+if __name__=='__main__':
+    app.run(debug=True)
