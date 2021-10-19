3ca7246596a287bfa47caa3b1e95c86f2053b6d0
monkeyapple <monkeyappleh@gmail.com>
2021-10-19 20:09:23 +0800

    database url

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,7 @@ app=Flask(__name__)
 
 app.config.from_mapping(
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1),
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
 
