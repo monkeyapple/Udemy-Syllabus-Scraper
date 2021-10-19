6ed2db0ccef122ea9c00268cfa8e1ffc6255e69f
monkeyapple <monkeyappleh@gmail.com>
2021-10-19 20:48:31 +0800

    procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python run.py migrate
 web: flask db upgrade; gunicorn run:app
--- project/__init__.py
+++ project/__init__.py
@@ -9,8 +9,8 @@ import os
 app=Flask(__name__)
 
 app.config.from_mapping(
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1),
+    SECRET_KEY = os.environ.get('SECRET_KEY'),
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
 
