d3f1290e3e75aca3ffef9940dfebe011a9147b9d
monkeyapple <monkeyappleh@gmail.com>
2021-10-20 15:26:25 +0800

    errorthe session is unavailable because no secret key was set.

--- project/__init__.py
+++ project/__init__.py
@@ -8,7 +8,7 @@ import os
 import pylibmc
 
 app=Flask(__name__)
-
+app.secret_key = os.urandom(24)
 app.config.from_mapping(
     SECRET_KEY = os.environ.get('SECRET_KEY'),
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1),
--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from project import app
 import os
 
 if __name__=='__main__':
-    app.secret_key = os.urandom(24)
     app.run()
