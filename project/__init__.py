d33b42029c9c19c74cd36745eb880f248992b94e
monkeyapple <monkeyappleh@gmail.com>
2023-02-21 17:42:04 +0800

    dockerize-01

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,8 @@ Talisman(app, content_security_policy=None)
 
 app.config.from_mapping(
     SECRET_KEY = os.environ.get('SECRET_KEY'),
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1),
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1),
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False, 
 )
 
@@ -20,6 +21,8 @@ app.config.from_mapping(
 
 db = SQLAlchemy(app)
 Migrate(app,db)
+
+db.create_all()
 
 from project.udemy.views import index_blueprint
 app.register_blueprint(index_blueprint,url_prefix='/')
