64eca5fd4640dba617284412f7991f2e234e7e47
monkeyapple <monkeyappleh@gmail.com>
2023-02-28 23:35:33 +0800

    add connect_unix,locally successful

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,14 +1,32 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# import pg8000
+
+
+# app=Flask(__name__)
+# # Talisman(app, content_security_policy=None)
+
+# app.config.from_object("project.config.Config")
+
+# db = SQLAlchemy(app)
+
+# from project.udemy.views import index_blueprint
+# app.register_blueprint(index_blueprint,url_prefix='/')
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pg8000
-
+from project.connect_unix import get_connect_url
+import os
 
 app=Flask(__name__)
 # Talisman(app, content_security_policy=None)
 
-app.config.from_object("project.config.Config")
+app.config["SQLALCHEMY_DATABASE_URI"] = get_connect_url()
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 db = SQLAlchemy(app)
+
 
 from project.udemy.views import index_blueprint
 app.register_blueprint(index_blueprint,url_prefix='/')
---
+++ services/web/project/connect_unix.py
@@ -0,0 +1,15 @@
+import os
+
+def get_connect_url():
+    db_user = os.environ["DB_USER"]  # e.g. 'my-database-user'
+    db_pass = os.environ["DB_PASS"]  # e.g. 'my-database-password'
+    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+    if "INSTANCE_UNIX_SOCKET" in os.environ:
+        unix_socket_path = os.environ["INSTANCE_UNIX_SOCKET"]  # e.g. '/cloudsql/project:region:instance'
+        return f"postgresql+pg8000://{db_user}:{db_pass}@/{db_name}?unix_sock={unix_socket_path}/.s.PGSQL.5432"
+    else:
+        # Local test
+        db_host=os.environ["DB_HOST"]
+        db_port=os.environ["DB_PORT"]
+        return f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+       
