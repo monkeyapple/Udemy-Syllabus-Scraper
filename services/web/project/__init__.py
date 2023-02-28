3dc5815341b185929a859b8782523118a2ea6aca
monkeyapple <monkeyappleh@gmail.com>
2023-02-28 15:56:56 +0800

    create_all in run

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -9,7 +9,6 @@ app=Flask(__name__)
 app.config.from_object("project.config.Config")
 
 db = SQLAlchemy(app)
-db.create_all()
 
 from project.udemy.views import index_blueprint
 app.register_blueprint(index_blueprint,url_prefix='/')
--- services/web/run.py
+++ services/web/run.py
@@ -1,5 +1,8 @@
-from project import app
+from project import app,db
 import os
+
+with app.app_context():
+    db.create_all()
 
 if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0",port=int(os.envion.get("PORT",8080)))
