f1ef448146d594e96d77d848eedf84f99c7ed193
monkeyapple <monkeyappleh@gmail.com>
2023-03-06 22:43:32 +0800

    fix

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,18 +1,3 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# import pg8000
-
-
-# app=Flask(__name__)
-# # Talisman(app, content_security_policy=None)
-
-# app.config.from_object("project.config.Config")
-
-# db = SQLAlchemy(app)
-
-# from project.udemy.views import index_blueprint
-# app.register_blueprint(index_blueprint,url_prefix='/')
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pg8000
